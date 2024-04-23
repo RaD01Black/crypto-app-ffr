3d9fb3efcf98cffc0628fa2bf8fbfcfcff1b6c31
Alereza_RaDBlaCK01 <aliireza01ahmadi@gmail.com>
2024-04-23 03:08:19 +0200

    Making chart with api

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react"
 import styles from "./chart.module.css"
+import { convertData } from "../../helpers/convertData"
+import { CartesianGrid, ResponsiveContainer, LineChart, Line, YAxis, XAxis, Tooltip, Legend  } from "recharts"
 
 function Chart({ chart, setChart }) {
+    const [type, setType] = useState("prices")
   return (
     <div className={styles.container}>
         <span className={styles.cross} onClick={() => setChart(null)}>X</span>
-        <div className={styles.chart}></div>
+        <div className={styles.chart}>
+          <div className={styles.graph}>
+            <ChartComponent data={convertData(chart, type)} type={type}/>
+          </div>
+        </div>
     </div>
   )
 }
 
 export default Chart
+
+const ChartComponent = ({data, type}) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+    <LineChart width={400} height={400} data={data}>
+      <Line type="monotone" dataKey={type} stroke="#3874ff" strokeWidth="2px" />
+      <CartesianGrid stroke="#404042" />
+      <YAxis dataKey={type} domain={["autpo" , "auto"]}/>
+      <XAxis dataKey="date" hide />
+      <Legend />
+      <Tooltip />
+    </LineChart>
+  </ResponsiveContainer>
+  );
+};
